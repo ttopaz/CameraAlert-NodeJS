299594d87a4fd82af7c1e2d103e564b65ce2bcec
Tony Topaz <ttopaz@gmail.com>
2018-08-19 02:50:45 -0400

    Adding FCM

--- app.js
+++ app.js
@@ -12,10 +12,22 @@ const json = require('express-json');
 const errorHandler = require('errorhandler');
 const ringApi = require( 'doorbot' );
 const watch = require('node-watch');
+const admin = require("firebase-admin");
+
+var serviceAccount = require("./camera-alert-12580-firebase-adminsdk-cgeu0-6a7ad66e8a.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://camera-alert-12580.firebaseio.com"
+});
+
+var registrationTokens = [
+    'fQ0CPq2uNbw:APA91bF-tTtWR5W6cXtTkyExJF36AgC29nmhTjQJk0i42vqqOkLSHecBi4zJ2opcvH95d-1Rscs-HlugJDqONJC04cq5MYtQNQ-BSGCa3nt5zWpHxDXTKXQtcKicV185kIlQI4F7rSG5aBN1_SQkakq9xXb9E93kGQ'
+];
 
 const app = express();
 
-const currentDevice = 0;
+const currentDevice = 0;    
 
 http.globalAgent.maxSockets = 1000;
 
@@ -38,8 +50,6 @@ var auth = (req, res, next) => {
         res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
         return res.send(401);
     };
-
-    console.log("request");
 
     var user = basicAuth(req);
 
@@ -122,12 +132,15 @@ app.get('/GetEvents', auth, (req, res) => {
     var ret = [];
 
     Object.keys(cameras).forEach((key) => {
-        if (cameras[key].changed != undefined)
+        var camera = cameras[key];
+        if (camera.changed != null)
         {
-            ret.push({ cameraId: key, cameraName: cameras[key].name, file: cameras[key].changed.file, date: cameras[key].changed.date });            
+            ret.push({ cameraId: key, cameraName: camera.name, file: camera.changed.file, date: camera.changed.date.toISOString() });            
+            camera.changed = null;
         }
     });
 
+    console.log(ret);
     res.send(ret);
 });
 
@@ -297,15 +310,51 @@ http.createServer(app).listen(app.get('port'), () => {
 
 console.log("Loading Camera data...");
 
+function sendMessage(camera, file, date)
+{
+    var payload = {
+        notification: {
+            title: 'Camera Alert',
+            body : 'Motion detected at ' + camera.name + ' on ' + date.toLocaleString()
+        },
+        data : {
+            cameraId : camera.Id,
+            cameraName : camera.name,
+            file : file,
+            date : date.toISOString()
+        }
+    };
+
+    admin.messaging().sendToDevice(registrationTokens, payload)
+    .then((response) => {
+        console.log('Sent successfully.\n');
+        console.log(response);
+    })
+    .catch((error) => {
+        console.log('Sent failed.\n');
+        console.log(error);
+    });
+}
+
 fs.readFile(__dirname + '/cameras.json', (err, data) => {
     cameras = JSON.parse(data);
 
     Object.keys(cameras).forEach((key) => {
-        if (cameras[key].type == "ftp")
+        var camera = cameras[key];
+        if (camera.type == "ftp")
         {
-            watch(cameras[key].files, { recursive: true }, function(evt, name) {
-                cameras[key].results = loadFtpFiles(key, cameras[key].files, 1, ".mp4");    
-                cameras[key].changed = { file: name.substring(cameras[key].files.length + 1), date: new Date().getTime() };
+            camera.lastChanged = null;
+            camera.results = null;
+            camera.changed = null;
+            watch(camera.files, { recursive: true }, function(evt, name) {
+                if (camera.lastChanged != name)
+                {
+                    console.log('%s changed', name);
+                    camera.results = loadFtpFiles(key, camera.files, 1, ".mp4");    
+                    camera.changed = { file: name.substring(camera.files.length + 1), date: new Date() };
+                    camera.lastChanged = name;
+                    sendMessage(camera, camera.changed.file, camera.changed.date);
+                }
             });
         }
     });
